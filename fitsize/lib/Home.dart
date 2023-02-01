960e01563503a4b6397ccf67c6dd104c67456b09
Sami <sami.ifakiren@gmail.com>
2023-02-01 16:31:14 +0100

    Navigation bar + camera visible

---
+++ fitsize/lib/Home.dart
@@ -0,0 +1,61 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<CameraDescription> cameras;
+  late CameraController cameraController;
+
+  @override
+  void initState() {
+    startCamera();
+    super.initState();
+  }
+
+  @override
+  void startCamera() async {
+    cameras = await availableCameras();
+    cameraController = CameraController(
+      cameras[0],
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+
+    await cameraController.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext) {
+    if (cameraController.value.isInitialized) {
+      return Scaffold(
+        body: Stack(
+          children: [
+            CameraPreview(cameraController),
+          ],
+        ),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+}
