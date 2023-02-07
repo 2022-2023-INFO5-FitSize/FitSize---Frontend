e0528a7adf0fbc7e494544f7547aec4750e34405
clementnguyen31 <clementnguyen31@gmail.com>
2023-02-07 16:12:27 +0100

    adding description on detail page

--- fitsize/lib/detailClothes.dart
+++ fitsize/lib/detailClothes.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 
-class DetailClothesPage extends StatelessWidget {
-
+class DetailClothesPage extends StatefulWidget {
   final String image;
 
   const DetailClothesPage({super.key, required this.image});
 
   @override
-  Widget build(BuildContext context) {
+  State<DetailClothesPage> createState() => _DetailsCLothesState();
+}
 
+class _DetailsCLothesState extends State<DetailClothesPage> {
+  late String size = "XS";
+  late String name = "T shirt";
+  late String brand = "Uniqlo";
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Détail"),
       ),
       body: Center(
-        child: Image (
-            image: AssetImage(image),
+        child: Column(
+          children: [
+            Container(
+                height: 500,
+                width: 500,
+                child:
+                    Image(image: AssetImage(widget.image), fit: BoxFit.cover)),
+            Text("nom: $name"),
+            Text("marque: $brand"),
+            Text("taille: $size")
+          ],
         ),
       ),
     );
   }
 }
-
