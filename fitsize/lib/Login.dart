09e81d3e26b5d1c25397beaa6c4ee3feec8f8617
clementnguyen31 <clementnguyen31@gmail.com>
2023-02-26 16:37:04 +0100

    traitement requete connexion

--- fitsize/lib/Login.dart
+++ fitsize/lib/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'main.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,6 +10,31 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  late String email;
+  late String password;
+
+  Future<void> submitForm() async {
+    if (formKey.currentState != null) {
+      if (formKey.currentState!.validate()) {
+        formKey.currentState!.save();
+        try {
+          final response = await http.get(
+            Uri.parse("http://10.0.2.2:8000/polls/user?login=$email&password=$password"),
+          );
+          print(response.body);
+          if (response.statusCode == 200) {
+            print("ok");
+          } else {
+            print("erreur");
+          }
+        } catch (e) {
+          // error
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +42,8 @@ class _LoginPageState extends State<LoginPage> {
           title: const Text('Connexion'),
         ),
         body: Center(
+            child: Form(
+          key: formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -29,18 +58,27 @@ class _LoginPageState extends State<LoginPage> {
               ),
               Container(
                 padding: const EdgeInsets.fromLTRB(40, 30, 40, 0),
-                child: TextField(
+                child: TextFormField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(0.0),
                     ),
                     labelText: 'E-mail',
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Entrez votre e-mail';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    email = value!;
+                  },
                 ),
               ),
               Container(
                 padding: const EdgeInsets.fromLTRB(40, 20, 40, 0),
-                child: TextField(
+                child: TextFormField(
                   obscureText: true,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
@@ -48,6 +86,15 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     labelText: 'Mot de passe',
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Entrez votre mot de passe';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    password = value!;
+                  },
                 ),
               ),
               Container(
@@ -58,8 +105,8 @@ class _LoginPageState extends State<LoginPage> {
                     style: ElevatedButton.styleFrom(
                       minimumSize: const Size.fromHeight(50),
                     ),
+                    onPressed: submitForm,
                     child: const Text('Connexion'),
-                    onPressed: () {},
                   )),
               TextButton(
                 onPressed: () {},
@@ -70,6 +117,6 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ],
           ),
-        ));
+        )));
   }
 }
