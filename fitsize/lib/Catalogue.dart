26bfdf9fc93ef3d94f9c8d092414e0f73a235c1d
clementnguyen31 <clementnguyen31@gmail.com>
2023-03-03 10:24:15 +0100

    création catalogue dynamique

--- fitsize/lib/Catalogue.dart
+++ fitsize/lib/Catalogue.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 import 'UserProvider.dart';
 import 'detailClothes.dart';
@@ -15,10 +18,42 @@ class _CataloguePageState extends State<CataloguePage> {
   late PageController _pageController;
   int currentPage = 0;
 
+  List<String> names = [];
+
   @override
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: currentPage);
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final login = userProvider.user.login;
+    fetchData(login);
+  }
+
+  // Fonction qui récupère le mot de passe dans un Json
+  List<String> getNameFromJson(List<dynamic> jsonList) {
+    List<String> names = [];
+    for (var item in jsonList) {
+      final name = item['name'];
+      if (name != null && name.isNotEmpty) {
+        names.add(name);
+      }
+    }
+    return names;
+  }
+
+  Future<void> fetchData(String login) async {
+    final response = await http
+        .get(Uri.parse('http://10.0.2.2:8000/polls/usermodel/login/$login'));
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      final names = getNameFromJson(jsonData);
+      setState(() {
+        this.names = names;
+      });
+    } else {
+      throw Exception('Failed to load names');
+    }
   }
 
   @override
@@ -27,12 +62,8 @@ class _CataloguePageState extends State<CataloguePage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    final userProvider = Provider.of<UserProvider>(context);
-    final userId = userProvider.user.id;
-    print(userId);
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.white,
@@ -51,6 +82,16 @@ class _CataloguePageState extends State<CataloguePage> {
               ),
             ],
           )),
+      body: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          final name = names[index];
+          return ElevatedButton(
+            child: Text(name),
+            onPressed: () {},
+          );
+        },
+      ),
     );
   }
 }
